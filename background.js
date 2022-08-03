1528aac0618da7e6f8ebba49f67500561ca66a41
Metin <metin.1464@gmail.com>
2022-08-03 21:46:17 +0300

    Deleted classes

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
-    if (changeInfo.url.toString().includes("shorts")) {
-        chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
+        if("url" in changeInfo)
+            if (changeInfo.url.includes("shorts")) {
+                chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
     }
 }
 );
