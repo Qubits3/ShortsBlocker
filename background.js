c40f9b3bfe9223fcd20da8a93a8db07599896609
Metin <metin.1464@gmail.com>
2022-08-04 14:44:03 +0300

    minimized code

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
     if ("url" in changeInfo) {
-        console.info(changeInfo.url);
-        if (changeInfo.url.includes("www.youtube.com/shorts")) {
+        if (changeInfo.url.includes("www.youtube.com/shorts"))
             chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
-        }
     }
 }
 );
