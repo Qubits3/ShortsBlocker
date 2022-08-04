1207a7a39079ff315d948488a5c4b24b7acf97bb
Metin <metin.1464@gmail.com>
2022-08-04 13:05:36 +0300

    improved comparison

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
-        if("url" in changeInfo)
-            if (changeInfo.url.includes("shorts")) {
-                chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
+    if ("url" in changeInfo) {
+        console.info(changeInfo.url);
+        if (changeInfo.url.includes("www.youtube.com/shorts")) {
+            chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
+        }
     }
 }
 );
