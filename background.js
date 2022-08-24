600417ba68f1a7e65eefe42a2f271326eff10fe6
Metin Yıldıran <metin.1464@gmail.com>
2022-08-24 18:22:16 +0300

    Added ability to return the last clicked link

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
-chrome.tabs.onUpdated.addListener(function
-    (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if ("url" in changeInfo) {
-        if (changeInfo.url.includes("www.youtube.com/shorts"))
-            chrome.tabs.update(tabId, { url: "https://www.youtube.com/feed/subscriptions" });
+        if (changeInfo.url.includes("www.youtube.com/shorts")) {
+            chrome.storage.sync.get(['key'], function (result) {
+                chrome.tabs.update(tabId, { url: result.key });
+            });
+        }
+        else
+        {
+            chrome.storage.sync.set({ key: changeInfo.url });
+        }
     }
 }
 );
